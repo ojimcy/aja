13ab33d40f1f96922c65ac19ff1c31570ffb378b
ojimcy <ojimcy247@gmail.com>
2023-02-28 16:16:26 +0100

    fetch products from backend

--- aja-fe/src/pages/HomePage.js
+++ aja-fe/src/pages/HomePage.js
@@ -1,37 +1,46 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import data from '../data'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+// import data from '../data';
 
 function HomePage() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('/api/products');
+      setProducts(result.data);
+    };
+    fetchData();
+  }, []);
   return (
     <div>
-        <h1>Featured Products</h1>
-          <div className="products">
-            {data.products.map((product) => (
-              <div
-                className="product"
-                key={product.slug}
-              >
-                <Link to={`/product/${product.slug}`}>
-                  <img
-                    src={product.image}
-                    alt={product.name}
-                  />
-                </Link>
-                <div className="product-info">
-                  <Link to={`/product/${product.slug}`}>
-                    <p>{product.name}</p>
-                  </Link>
-                  <strong>
-                    <p>{product.price}</p>
-                  </strong>
-                  <button>Add to cart</button>
-                </div>
-              </div>
-            ))}
+      <h1>Featured Products</h1>
+      <div className="products">
+        {products.map((product) => (
+          <div
+            className="product"
+            key={product.slug}
+          >
+            <Link to={`/product/${product.slug}`}>
+              <img
+                src={product.image}
+                alt={product.name}
+              />
+            </Link>
+            <div className="product-info">
+              <Link to={`/product/${product.slug}`}>
+                <p>{product.name}</p>
+              </Link>
+              <strong>
+                <p>{product.price}</p>
+              </strong>
+              <button>Add to cart</button>
+            </div>
           </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
