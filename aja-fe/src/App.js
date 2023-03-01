21abacbfcfd7e7c589e5fa3708542d94baea59b3
ojimcy <ojimcy247@gmail.com>
2023-03-01 23:01:59 +0100

    add bootsrapp and implement product ratings

--- aja-fe/src/App.js
+++ aja-fe/src/App.js
@@ -1,26 +1,44 @@
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ProductPage from './pages/ProductPage';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
+      <div className="d-flex flex-column site-container">
         <header className="App-header">
-          <Link to="/">Aja</Link>
+          <Navbar
+            bg="dark"
+            varient="dark"
+            color="white"
+          >
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand className="text-white">Aja</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
-          <Routes>
-            <Route
-              path="/"
-              element={<HomePage />}
-            />
-            <Route
-              path="/product/:slug"
-              element={<ProductPage />}
-            />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route
+                path="/"
+                element={<HomePage />}
+              />
+              <Route
+                path="/product/:slug"
+                element={<ProductPage />}
+              />
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className="text-center">All rights reserve</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
